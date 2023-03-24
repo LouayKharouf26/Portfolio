ceb4994e1e8f327145ba0932497a3ae4ee9b2348
louay kharouf <louay.khrouf@insat.ucar.tn>
2023-03-24 15:16:56 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
 
     <nav id="navbar" class="navbar nav-menu">
       <ul>
-        <li><a href="#hero" class="nav-link scrollto "><i class="bx bx-home"></i> <span>Home</span></a></li>
+        <li><a href="#home" class="nav-link scrollto "><i class="bx bx-home"></i> <span>Home</span></a></li>
         <li><a href="#about" class="nav-link scrollto"><i class="bx bx-user"></i> <span>About</span></a></li>
         <li><a href="#resume" class="nav-link scrollto"><i class="bx bx-file-blank"></i> <span>Resume</span></a></li>
         <li><a href="#portfolio" class="nav-link scrollto"><i class="bx bx-book-content"></i> <span>Projects</span></a></li>
@@ -30,7 +30,7 @@ function App() {
       </ul>
     </nav>
   </header>
-  <section id="hero" class="d-flex flex-column justify-content-center">
+  <section id="home" class="d-flex flex-column justify-content-center">
     <div class="container" data-aos="zoom-in" data-aos-delay="100">
       <h1>Louay Kharouf</h1>
       <p class="typewriter ">Network Engineering Student @ INSAT | CTF Player</p>
